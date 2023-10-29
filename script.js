aefa982ae665562256a9f17a54e3d5ca5c3194fc
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2023-10-28 21:02:02 -0400

    rps images display middle of screen

--- script.js
+++ script.js
@@ -91,6 +91,7 @@ function startRockPaperScissorGame(playerChoice1, playerChoice2) {
 	}
 
 	function twoPlayerGameMode() {
+		let playerTurn = 1;
 		const headerText = document.createElement("h1");
 		headerText.textContent = "Player 1 Choose";
 		headerText.classList.add("rps-header");
@@ -103,7 +104,7 @@ function startRockPaperScissorGame(playerChoice1, playerChoice2) {
 		scissorsDisplay.src = "Scissors.jpg";
 		rockDisplay.classList.add("rps-display-image");
 		paperDisplay.classList.add("rps-display-image");
-		scissorsDisplay.classList.add('rps-display-image')
+		scissorsDisplay.classList.add("rps-display-image");
 
 		const rpsDisplayContainer = document.createElement("div");
 		const image1Display = document.createElement("div");
@@ -165,20 +166,77 @@ function startRockPaperScissorGame(playerChoice1, playerChoice2) {
 		for (const button of buttons) {
 			button.addEventListener("click", () => {
 				const buttonId = button.id;
-				if (buttonId === "rock") {
+				let player1Choice = buttonId;
+				if (buttonId === "rock" && playerTurn === 1) {
+					player1Choice = buttonId;
+					headerText.textContent = "Player 2 Choose";
 					image1Display.append(rockDisplay);
 					rpsDisplayContainer.append(image1Display);
 					rockDisplay.style.display = "block";
+					setTimeout(() => {
+						playerTurn = 2;
+					}, 100);
 				}
-				if (buttonId === "paper") {
+				if (buttonId === "paper" && playerTurn === 1) {
+					headerText.textContent = "Player 2 Choose";
 					image1Display.append(paperDisplay);
+					
 					rpsDisplayContainer.append(image1Display);
 					paperDisplay.style.display = "block";
+					setTimeout(() => {
+						playerTurn = 2;
+					}, 100);
 				}
-				if (buttonId === "scissors") {
+				if (buttonId === "scissors" && playerTurn === 1) {
+					headerText.textContent = "Player 2 Choose";
 					image1Display.append(scissorsDisplay);
 					rpsDisplayContainer.append(image1Display);
 					scissorsDisplay.style.display = "block";
+					setTimeout(() => {
+						playerTurn = 2;
+					}, 100);
+				}
+				if (buttonId === "rock" && playerTurn === 2) {
+					if (player1Choice === "rock") {
+						const copyRockImage = new Image();
+						copyRockImage.src = "Rock.jpg";
+						copyRockImage.classList.add("rps-display-image");
+						image2Display.append(copyRockImage);
+						rpsDisplayContainer.append(image2Display);
+						copyRockImage.style.display = "block";
+					} else {
+						image2Display.append(rockDisplay);
+						rpsDisplayContainer.append(image2Display);
+						rockDisplay.style.display = "block";
+					}
+				}
+				if (buttonId === "paper" && playerTurn === 2) {
+					if (player1Choice === "paper") {
+						const copyPaperImage = new Image();
+						copyPaperImage.src = "Paper.jpg";
+						copyPaperImage.classList.add("rps-display-image");
+						image2Display.append(copyPaperImage);
+						rpsDisplayContainer.append(image2Display);
+						copyPaperImage.style.display = "block";
+					} else {
+						image2Display.append(paperDisplay);
+						rpsDisplayContainer.append(image2Display);
+						paperDisplay.style.display = "block";
+					}
+				}
+				if (buttonId === "scissors" && playerTurn === 2) {
+					if (player1Choice === "scissors") {
+						const copyScissorsImage = new Image();
+						copyScissorsImage.src = "Scissors.jpg";
+						copyScissorsImage.classList.add("rps-display-image");
+						image2Display.append(copyScissorsImage);
+						rpsDisplayContainer.append(image2Display);
+						copyScissorsImage.style.display = "block";
+					} else {
+						image2Display.append(scissors);
+						rpsDisplayContainer.append(image2Display);
+						scissorsDisplay.style.display = "block";
+					}
 				}
 			});
 		}
